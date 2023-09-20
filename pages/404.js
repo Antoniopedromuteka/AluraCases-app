e2a0c8835f97a81895ecb8babc1aba6be5c915f0
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2023-09-20 10:36:04 +0100

    feat: adicionando a estrutura de pastas do projecto

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link"
+
+export default function Page404(){
+  return(
+    <div>
+      <h1>Você se perdeu</h1> 
+      <Link href="/">Ir para a Home</Link>
+    </div>
+  )
+}
