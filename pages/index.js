a87169229085b401eb25802d18a5635fb910f368
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2023-09-20 11:05:44 +0100

    style(home): home feita

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,3 @@
-import Link  from "../src/components/Link"
+import HomeScreen from "../src/screens/HomeScreen"
 
-
-function Title ({ children, as }){
-  const Tag = as ?? 'h1'
-  return(
-    <>
-    <Tag>
-      { children }
-    </Tag>
-    <style jsx>{`
-      ${Tag} {
-        color: red;
-      }
-
-    `}</style>
-    </>
-  )
-}
-export default function HomePage(){
-  return(
-    <div>
-      <Title>Alura Cases - Home Page</Title> 
-      <Link href="/faq">Ir para o FAQ</Link>
-    </div>
-  )
-}
+export default HomeScreen
